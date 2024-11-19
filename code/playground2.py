65782f4eabdad886ea644603ae857a6db4ab66d1
KlaidasCodes <161773378+KlaidasCodes@users.noreply.github.com>
2024-11-19 16:13:46 +0000

    Update playground2.py
    More updates to the main_window:
    
    -Set up a mechanism that checks the player's level and gender and then picks out their avatar, places it in the needed location. 
    -Created an avatar path dictionary

--- code/playground2.py
+++ code/playground2.py
@@ -2,12 +2,50 @@ import os
 import binascii
 import hashlib
 import json
-from tkinter import *
+import tkinter as tk
 from PIL import Image, ImageTk
 
 
 LIGHT_ORANGE = "#ffbc5c"
 LIGHTER_ORANGE = "#EBDDC9"
+STAR_IMAGE_SRC = "./images/star2.png"
+PLACEHOLDER_EXP = 125
+PLACEHOLDER_PERSON = "Alice"
+
+
+avatars = {
+    "male": {
+        "1": "./images/Avatars/Male/male1.png",
+        "2-5": "./images/Avatars/Male/male2-5.png",
+        "5-10": "./images/Avatars/Male/male5-10.png",
+        "10-15": "./images/Avatars/Male/male10-15.png",
+        "15-30": "./images/Avatars/Male/male15-30.png",
+        "30+": "./images/Avatars/Male/male30+.png"
+    },
+    "female": {
+        "1": "./images/Avatars/Female/female1.png",
+        "2-5": "./images/Avatars/Female/female2-5.png",
+        "5-10": "./images/Avatars/Female/female5-10.png",
+        "10-15": "./images/Avatars/Female/female10-15.png",
+        "15-30": "./images/Avatars/Female/female15-30.png",
+        "30+": "./images/Avatars/Female/female30+.png"
+    }
+}
+
+def pick_avatars_path(level, sex, avatar_dict):
+    if level == 1:
+        return avatar_dict[sex]["1"]
+    elif 2 <= level < 5:
+        return avatar_dict[sex]["2-5"]
+    elif 5 <= level < 10:
+        return avatar_dict[sex]["5-10"]
+    elif 10 <= level < 15:
+        return avatar_dict[sex]["10-15"]
+    elif 15 <= level < 30:
+        return avatar_dict[sex]["15-30"]
+    else:
+        return avatar_dict[sex]["30+"]
+
 
 def draw_rounded_rectangle(canvas, x1, y1, x2, y2, radius=30, **kwargs):
     """takes input top left and bottom right corners (coordinates relative to parent's top left corner)
@@ -27,11 +65,16 @@ def close_window(window):
     window.quit()
 
 
+PLACEHOLDER_LEVEL = 2
+PLACEHOLDER_GENDER = "female"
+
 
 with open("./database_main.json", mode="r+") as file:
             main_database = json.load(file)
 
-main_window = Tk()
+person_logged_in = main_database["Alice"]
+
+main_window = tk.Tk()
 
 main_window.config(width=800, height=600, bg=LIGHT_ORANGE)
 main_window.minsize(width=800, height=600)
@@ -39,29 +82,49 @@ for i in range(20):
     main_window.columnconfigure(i, weight=1)
     main_window.rowconfigure(i, weight=1)
 
-corner = Canvas(main_window, width=1, height=1, bg=LIGHT_ORANGE, bd=0, highlightthickness=0)
+corner = tk.Canvas(main_window, width=1, height=1, bg=LIGHT_ORANGE, bd=0, highlightthickness=0)
 corner.grid(column=0, row=0)
 
-main_left_big_rectangle_canvas = Canvas(main_window, width=300, height=420, bg=LIGHT_ORANGE, bd=0, highlightthickness=0)
+main_left_big_rectangle_canvas = tk.Canvas(main_window, width=300, height=420, bg=LIGHT_ORANGE, bd=0, highlightthickness=0)
 main_left_big_rectangle_canvas.grid(column=1, columnspan=8, row=3, rowspan=14, sticky="w", padx=20)
 main_left_big_rectangle_canvas_rectangle = draw_rounded_rectangle(main_left_big_rectangle_canvas, 0, 0, 300, 420, fill=LIGHTER_ORANGE)
 
-main_left_small_rectangle_canvas = Canvas(main_window, width=300, height=60, bg=LIGHT_ORANGE, bd=0, highlightthickness=0)
+
+main_left_small_rectangle_canvas = tk.Canvas(main_window, width=300, height=60, bg=LIGHT_ORANGE, bd=0, highlightthickness=0)
 main_left_small_rectangle_canvas.grid(column=1, columnspan=8, row=17, rowspan=2, sticky="nw", padx=20, pady=20)
 main_left_small_rectangle_canvas_rectangle = draw_rounded_rectangle(main_left_small_rectangle_canvas, 0, 0, 300, 60, radius=15, fill=LIGHTER_ORANGE)
 
-main_right_big_rectangle_canvas = Canvas(main_window, width=420, height=420, bg=LIGHT_ORANGE, bd=0, highlightthickness=0)
+main_right_big_rectangle_canvas = tk.Canvas(main_window, width=420, height=420, bg=LIGHT_ORANGE, bd=0, highlightthickness=0)
 main_right_big_rectangle_canvas.grid(column=9, columnspan=11, row=3, rowspan=14, sticky="e", padx=20)
 main_right_big_rectangle_canvas_rectangle = draw_rounded_rectangle(main_right_big_rectangle_canvas, 0, 0, 420, 420, fill=LIGHTER_ORANGE)
 
-main_name_canvas = Canvas(main_window, width=200, height=60, bg=LIGHTER_ORANGE, bd=0, highlightthickness=0)
+main_name_canvas = tk.Canvas(main_window, width=200, height=60, bg=LIGHTER_ORANGE, bd=0, highlightthickness=0)
 main_name_canvas.grid(column=2, columnspan=6, row=14, rowspan=2)
-main_name_canvas.create_text(100, 30, text="Maximus", font=("Futura Display", 44))
+main_name_canvas.create_text(100, 30, text=PLACEHOLDER_PERSON, font=("Futura Display", 38))
+
+
+main_left_top_small_rectangle_canvas = tk.Canvas(main_window, width=300, height=80, bg=LIGHT_ORANGE, 
+                                                 bd=0, highlightthickness=0)
+main_left_top_small_rectangle_canvas.grid(column=0, columnspan=8, row=0, rowspan=2)
+star_image = Image.open(STAR_IMAGE_SRC)
+star_image_smaller = star_image.resize((50, 50), Image.LANCZOS)
+star_image_tk = ImageTk.PhotoImage(star_image_smaller)
+main_left_top_small_rectangle_canvas.create_image(50, 50, image=star_image_tk)
+main_left_top_small_rectangle_canvas.image = star_image_tk
+main_left_top_small_rectangle_canvas.create_text(160, 50, text=f"Total Exp: {PLACEHOLDER_EXP}", font=("Futura Display", 24))
+
+
+# pick avatar according to level and place it on the main left big rectangle
+avatar_path = pick_avatars_path(PLACEHOLDER_LEVEL, PLACEHOLDER_GENDER, avatars)
+avatar_semi = Image.open(avatar_path)
+avatar_semi_resized = avatar_semi.resize((280, 340), Image.LANCZOS)
+avatar_image = ImageTk.PhotoImage(avatar_semi_resized)
+main_left_big_rectangle_canvas.create_image(150, 210, image=avatar_image)
+main_left_big_rectangle_canvas.image = avatar_image
 
 
 
 
 
-
-main_window.after(2000, lambda: close_window(main_window))
-mainloop()
+# main_window.after(2000, lambda: close_window(main_window))
+tk.mainloop()
